389e06ec93b0d78a32a60acbcf26c828747c9038
Edberaga <72023119+Edberaga@users.noreply.github.com>
2026-06-04 16:43:53 +0700

    Change icons

--- app.config.js
+++ app.config.js
@@ -13,7 +13,7 @@ module.exports = {
     slug: "wegood4u-mobile",
     version: pkg.version,
     orientation: "portrait",
-    icon: "./assets/images/icon.png",
+    icon: "./assets/images/icon.jpeg",
     scheme: "wegood4u",
     userInterfaceStyle: "light",
     splash: {
